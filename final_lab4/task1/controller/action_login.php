c25de2680fd629f67e2564b71b916adbf9c96da2
Fahmee Faiza <53439873+fahmee-faiza@users.noreply.github.com>
2020-11-29 22:29:22 +0600

    Update action_login.php

--- final_lab4/task1/controller/action_login.php
+++ final_lab4/task1/controller/action_login.php
@@ -2,20 +2,19 @@
 include '../task1/model/dbcon.php';
 
  if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
       
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['psw']); 
       
-      $sql = "SELECT user FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT user id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($conn,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
       
       $count = mysqli_num_rows($result);
 
       if($count ==1) {
       	echo "Login Successful"";
+       header("Location: /task1/view/balance.php");
+       exit();
       }
       else {
       	$error = "Invalid name or id or password";
